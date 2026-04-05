44456ab1433a565f2db5341a1e26985e9d12de5b
Sneha Jain <jainsnehasj6@gmail.com>
2026-04-05 20:33:44 +0530

    Initialized Project

---
+++ src/main/java/com/sneha/code_debug_assistant/CodeDebugAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.code_debug_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDebugAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDebugAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sneha/code_debug_assistant/CodeDebugAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.code_debug_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDebugAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
